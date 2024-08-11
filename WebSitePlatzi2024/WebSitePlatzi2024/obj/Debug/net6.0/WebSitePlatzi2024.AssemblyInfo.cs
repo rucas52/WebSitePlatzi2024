98eb01db16ea85a6a097f38ade1c2b02e84899c8
rcastillo <rucas52@hotmail.com>
2024-08-11 15:18:25 -0500

    Proyectd modificado

--- WebSitePlatzi2024/WebSitePlatzi2024/obj/Debug/net6.0/WebSitePlatzi2024.AssemblyInfo.cs
+++ WebSitePlatzi2024/WebSitePlatzi2024/obj/Debug/net6.0/WebSitePlatzi2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSitePlatzi2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b89b6459a3c2a0205da0bc9fe7e8e683c5b73af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5259958f9d8fbcf41be7f2dda3f91adab91cf845")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSitePlatzi2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSitePlatzi2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
